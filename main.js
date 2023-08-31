664ba383b99ae0895023cbf3db46d9dcdfc1f84b
CanAu21 <furkan.adrian9@outlook.com>
2023-08-31 15:10:59 +0300

    SCSS ile Landing Page Yapıldı

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".menu-toggle");
+const menuSide = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click",()=>{
+    menuSide.classList.toggle("open")
+})
